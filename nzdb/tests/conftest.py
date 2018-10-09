a301cc0d766d01eb350856231f6a0449b37f1223
Art Goldhammer <art.goldhammer@gmail.com>
2018-10-09 17:31:14 -0400

    added setup fixture to test

---
+++ nzdb/tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+import os
+import sys
+from subprocess import call
+
+config_file = os.getenv("NZDBCONF")
+if "localtest.conf" not in config_file:
+    print("For testing, NZDBCONF must be set to ~/Prog/nooze/confs/localtest.conf")  # noqa
+    sys.exit(255)
+
+topdir = os.path.expanduser('~/Prog/nooze/')
+
+testenv = {'NZDBCONF': config_file}
+print(f"Test environment {testenv}")
+print(f"topdir is {topdir}")
+
+
+@pytest.fixture(autouse=True, scope='session')
+def setup_testdb():
+    # setup
+    print("running setup")
+    wd = os.getcwd()
+    print(f"wd = {wd}")
+    os.chdir(topdir)
+    # run(["./test_setup"], env=testenv,
+    #     shell=True, check=True, capture_output=True)
+    call("./test_setup", shell=True)
+    os.chdir(wd)
+    yield
+
+    # teardown
+    from nzdb.dbconnect import twitterdb
+    twitterdb.db.client.drop_database('localtest')

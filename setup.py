2d4456a819ebb81b439fa312d123228a200c546d
Art Goldhammer <art.goldhammer@gmail.com>
2020-08-17 13:31:04 -0400

    updated setup.py bumped ver to 0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 setup(name="nzdb",
-      version="0.1",
+      version="0.2",
+      author="Art Goldhammer",
+      description="News Aggregator Web App",
+      author_email="art.goldhamme@gmail.com",
+      keywords="news aggregator Europe politics",
+      url="https://github.com/agoldhammer/nooze",
       scripts=['nzdb/bin/query', 'nzdb/bin/maketopics', 'nzdb/bin/readfeed',
                'nzdb/bin/storeauthtable'],
       packages=find_packages())
